d54e2fe32a4ccf9e87bf24a17ffe9773ef9f588c
DiegoAGomezS <dagomez@uamv.edu.ni>
2025-05-06 10:04:07 -0600

    Ej 3

--- Ejercicios/Ejercicio3.py
+++ Ejercicios/Ejercicio3.py
@@ -1,1 +1,12 @@
-Palabra = input("Ingrese una palabra")
+def contador(cadena):
+    vocales = ["a", "e","i", "o", "u"]
+    total = 0
+    for char in vocales:
+        total += 1
+    return total
+
+def main():
+    cadena = input("Ingrese el enunciadi")
+    print(f"La cadena contiene {contador(cadena)} vocales")
+
+main()
